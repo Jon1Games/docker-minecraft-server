703b2e49c466ebe5f004d49547fd92430aa06cd4
Jon1Games <jonas.saslona@gmx.de>
2024-05-15 22:34:26 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM eclipse-temurin:21-jre
+
+VOLUME ["/data"]
+WORKDIR /data
+
+RUN apt update
+RUN apt -y install jq wget
+
+COPY --chmod=755 scripts/start.sh /
+
+EXPOSE 25565
+
+CMD /start.sh
---
+++ scripts/start.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+if [ -z "${jarType}" ]; then 
+	echo "Unknows jar type, use one of these:"
+	echo " -> paper, vanilla"
+	exit 1
+fi
+
+if [ -z "${version}" ]; then
+	echo "Version angeben (1.20.4)"
+	exit 1
+fi
+
+case "$jarType" in
+    paper)
+        LATEST_BUILD=$(wget -O - -q https://api.papermc.io/v2/projects/paper/versions/${version}/builds | \
+                jq -r '.builds | map(select(.channel == "default") | .build) | .[-1]')
+
+        if [ -z "$LATEST_BUILD" ]; then
+            echo "Invalid version"
+            exit 1
+        fi
+        if [ "$LATEST_BUILD" = "null" ]; then
+            echo "No stable build for version $version found :("
+            exit 2
+        fi
+        rmident=paper
+        jarFile=${rmident}-${version}-${LATEST_BUILD}.jar
+        url="https://api.papermc.io/v2/projects/paper/versions/${version}/builds/${LATEST_BUILD}/downloads/${jarFile}"
+        ;;
+    
+     vanilla)
+	versionurl=$(wget -O - -q https://piston-meta.mojang.com/mc/game/version_manifest_v2.json | \
+		jq -r ".versions | map(select(.id == \"$version\") | .url) | .[0]")
+	url=$(wget -O - -q $versionurl | jq -r '.downloads.server.url')
+	rmident=vanilla
+	jarFile=${rmident}-${version}.jar
+	;;
+
+     purpur)
+	LATEST_BUILD=$(wget -O - -q https://api.purpurmc.org/v2/purpur/${version}/ | \
+		jq -r ".builds.latest")
+	
+        if [ -z "$LATEST_BUILD" ]; then
+            echo "Invalid version"
+            exit 1
+        fi
+        if [ "$LATEST_BUILD" = "null" ]; then
+            echo "No stable build for version $version found :("
+            exit 2
+        fi
+
+	url=https://api.purpurmc.org/v2/purpur/${version}/${LATEST_BUILD}/download
+	rmident=purpur
+	jarFile=${rmident}-${version}-${LATEST_BUILD}.jar
+	;;
+
+    *)
+	echo "Unknows jar type, use one of these:"
+	echo " -> paper, vanilla"
+        exit 1
+        ;;
+esac
+
+if ! [ -f "$jarFile" ]; then
+    rm -f ${rmident}-*.jar
+    if ! wget -O "$jarFile" -q "$url"; then
+        echo "Could not download server file"
+        exit 2
+    else
+        echo "Download successful"
+    fi
+fi
+
+if [ "$EULA" = "true" ]; then 
+	echo "eula=true" > ./eula.txt
+	exec java -Xmx${maxRam}G -jar "$jarFile" -nogui
+else
+	echo "eula=false" > ./eula.txt
+        echo "Accept the eula"
+	echo "enviroment variable: EULA=true"
+fi	
