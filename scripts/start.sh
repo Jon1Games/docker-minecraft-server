3e76e8135a07eda2faacd49d687900566c9203e2
jonas <jonas.saslona@gmx.de>
2024-11-19 21:47:40 +0100

    fabric support

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# ENV: EULA, jarType, Version, BUILD(not for Vanilla)
+
 if [ -z "${jarType}" ]; then 
 	echo "Unknows jar type, use one of these:"
 	echo " -> vanilla, paper, purpur"
+    echo " -> TODO: Forge, NeoForge, Fabrik"
 	exit 1
 fi
 
@@ -11,72 +14,122 @@ if [ -z "${version}" ]; then
 	exit 1
 fi
 
-case "$jarType" in
-    paper)
-        LATEST_BUILD=$(wget -O - -q https://api.papermc.io/v2/projects/paper/versions/${version}/builds | \
-                jq -r '.builds | map(select(.channel == "default") | .build) | .[-1]')
+# Check if wisched jar already exist, if set to ${jarFile} to allow starting
+## Check for Vanilla Jar
+if [ -e "${jarType}-${version}.jar" ]; then
+    jarFile="${jarType}-${version}.jar"
+    echo "using existing Jar"
+## Check for Paper,PurPur,Fabric Jar
+elif [ -e "${jarType}-${version}-${BUILD}.jar" ]; then
+    jarFile="${jarType}-${version}-${BUILD}.jar"
+    echo "using existing Jar"
+# Download Jar
+else
+    case "$jarType" in
+        paper)
+            if [ "$BUILD" = "latest"]; then
+                BUILD=$(wget -O - -q https://api.papermc.io/v2/projects/paper/versions/${version}/builds | \
+                    jq -r '.builds | map(select(.channel == "default") | .build) | .[-1]')
+            
+                if [ -z "$BUILD" ]; then
+                    echo "Invalid version"
+                    exit 1
+                fi
+                if [ "$BUILD" = "null" ]; then
+                    echo "No stable build for version $version found :("
+                    exit 2
+                fi
+            fi
+            rmident=paper
+            jarFile=${rmident}-${version}-${BUILD}.jar
+            url="https://api.papermc.io/v2/projects/paper/versions/${version}/builds/${BUILD}/downloads/${jarFile}"
+            ;;
+        
+        vanilla)
+            versionurl=$(wget -O - -q https://piston-meta.mojang.com/mc/game/version_manifest_v2.json | \
+                jq -r ".versions | map(select(.id == \"$version\") | .url) | .[0]")
+            url=$(wget -O - -q $versionurl | jq -r '.downloads.server.url')
+            rmident=vanilla
+            jarFile=${rmident}-${version}.jar
+            ;;
 
-        if [ -z "$LATEST_BUILD" ]; then
-            echo "Invalid version"
+        purpur)
+            if [ "$BUILD" = "latest"]; then
+                LATEST_BUILD=$(wget -O - -q https://api.purpurmc.org/v2/purpur/${version}/ | \
+                    jq -r ".builds.latest")
+            
+                if [ -z "$LATEST_BUILD" ]; then
+                    echo "Invalid version"
+                    exit 1
+                fi
+                if [ "$LATEST_BUILD" = "null" ]; then
+                    echo "No stable build for version $version found :("
+                    exit 2
+                fi
+            fi
+            url=https://api.purpurmc.org/v2/purpur/${version}/${LATEST_BUILD}/download
+            rmident=purpur
+            jarFile=${rmident}-${version}-${LATEST_BUILD}.jar
+            ;;
+
+        fabric)
+            if [ "$BUILD" = "latest"]; then
+                LATEST_BUILD=$(wget -O - -q https://meta.fabricmc.net/v2/versions/loader/${version}/ | \
+                    jq -r ".[0].loader.version")
+            
+                if [ -z "$LATEST_BUILD" ]; then
+                    echo "Invalid version"
+                    exit 1
+                fi
+                if [ "$LATEST_BUILD" = "null" ]; then
+                    echo "No stable build for version $version found :("
+                    exit 2
+                fi
+            fi
+            INSTALLER=$(wget -O - -q https://meta.fabricmc.net/v2/versions/installer/ | \
+                    jq -r ".[0].version")
+            url=https://meta.fabricmc.net/v2/versions/loader/${version}/${LATEST_BUILD}/${INSTALLER}/server/jar
+            rmident=fabric
+            jarFile=${rmident}-${version}-${LATEST_BUILD}.jar
+            ;;
+
+        *)
+        echo "Unknows jar type, use one of these:"
+        echo " -> vanilla, paper, purpur"
+        echo " -> TODO: Forge, NeoForge, Fabrik"
             exit 1
-        fi
-        if [ "$LATEST_BUILD" = "null" ]; then
-            echo "No stable build for version $version found :("
+            ;;
+    esac
+
+    if ! [ -f "$jarFile" ]; then
+        rm -f ${rmident}-*.jar
+        if ! wget -O "$jarFile" -q "$url"; then
+            echo "Could not download server file, have you checked if the build exist?"
             exit 2
+        else
+            echo "Download successful"
         fi
-        rmident=paper
-        jarFile=${rmident}-${version}-${LATEST_BUILD}.jar
-        url="https://api.papermc.io/v2/projects/paper/versions/${version}/builds/${LATEST_BUILD}/downloads/${jarFile}"
-        ;;
-    
-    vanilla)
-        versionurl=$(wget -O - -q https://piston-meta.mojang.com/mc/game/version_manifest_v2.json | \
-            jq -r ".versions | map(select(.id == \"$version\") | .url) | .[0]")
-        url=$(wget -O - -q $versionurl | jq -r '.downloads.server.url')
-        rmident=vanilla
-        jarFile=${rmident}-${version}.jar
-        ;;
-
-    purpur)
-        LATEST_BUILD=$(wget -O - -q https://api.purpurmc.org/v2/purpur/${version}/ | \
-            jq -r ".builds.latest")
-	
-        if [ -z "$LATEST_BUILD" ]; then
-            echo "Invalid version"
-            exit 1
-        fi
-        if [ "$LATEST_BUILD" = "null" ]; then
-            echo "No stable build for version $version found :("
-            exit 2
-        fi
-
-        url=https://api.purpurmc.org/v2/purpur/${version}/${LATEST_BUILD}/download
-        rmident=purpur
-        jarFile=${rmident}-${version}-${LATEST_BUILD}.jar
-        ;;
-
-    *)
-	echo "Unknows jar type, use one of these:"
-	echo " -> vanilla, paper, purpur"
-        exit 1
-        ;;
-esac
-
-if ! [ -f "$jarFile" ]; then
-    rm -f ${rmident}-*.jar
-    if ! wget -O "$jarFile" -q "$url"; then
-        echo "Could not download server file"
-        exit 2
-    else
-        echo "Download successful"
     fi
 fi
 
 if [ "$EULA" = "true" ]; then 
 	echo "eula=true" > ./eula.txt
-	exec java -jar "$jarFile" -nogui
+    # different start arguments for different server types
+    case "$jarType" in
+        vanilla)
+            exec java -jar "$jarFile" -nogui
+            ;;
+        paper)
+            exec java -jar "$jarFile" -nogui
+            ;;
+        purpur)
+            exec java -jar "$jarFile" -nogui
+            ;;
+        fabric)
+            exec java -jar "$jarFile" -nogui
+            ;;
 else
 	echo "eula=false" > ./eula.txt
-        echo "Accept the eula"
+    echo "Accept the eula"
 	echo "enviroment variable: EULA=true"
 fi	
